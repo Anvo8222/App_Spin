e23fbfb8cda459f57509c4f82b18884ba56ed579
Nguyen An <anvo8222@gmail.com>
2022-03-25 17:06:09 +0700

    update

--- src/utils/string.js
+++
@@ -1,9 +0,0 @@
-export const checkEnoughWords = (string = '') => {
-  if (string.toLowerCase().trim().split(' ').length === 2) {
-    return true;
-  }
-  return false;
-};
-
-export const getLastWord = (string = '') =>
-  string.trim().split(' ')[0] || string;
